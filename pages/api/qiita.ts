7307e6d0d75c124451cc0944fe36118212c65fb7
AFURO-1130 <abcshotaro616@gmail.com>
2024-04-14 02:45:28 +0900

    feat: qiitaAPIからきいを取得してきて、microCMSから記事をしてきたときのUIを整えた

---
+++ pages/api/qiita.ts
@@ -0,0 +1,20 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  name: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+  // qiitaAPIからデータを取得する処理
+  const response = await fetch("https://qiita.com/api/v2/authenticated_user/items", {
+    headers: {
+      Authorization: `Bearer ${process.env.QIITA_TOKEN}`,
+    },
+  });
+  const data = await response.json();
+  console.log({ data })
+  res.status(200).json(data);
+}
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -5,23 +5,41 @@ import Layout from "../layout";
 export default function Blog({ blog }: { blog: BlogPost[] }) {
   return (
     <Layout title="Blog">
-      <ul className="list-disc">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         {blog.map((blog) => (
-          <li key={blog.id}>
-            <Link href={`/blog/${blog.id}`}>{blog.title}</Link>
-          </li>
+          <div
+            key={blog.id}
+            className="max-w-sm rounded overflow-hidden shadow-lg"
+          >
+            <img className="w-full" src={blog.heroImage.url} alt="Blog image" />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">{blog.title}</div>
+              <p className="text-gray-700 text-base">{blog.description}</p>
+            </div>
+            <div className="px-6 pt-4 pb-2">
+              <Link href={`/blog/${blog.id}`}>
+                <p className="inline-block bg-blue-500 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
+                  Read More
+                </p>
+              </Link>
+            </div>
+          </div>
         ))}
-      </ul>
+      </div>
     </Layout>
   );
 }
 // データをテンプレートに受け渡す部分の処理を記述します
 export const getStaticProps = async () => {
-  const data = await client.get({ endpoint: "blogs" });
-
+  const blogs = await client.get({ endpoint: "blogs" });
+  // apiRouteで作成したエンドポイントを指定してデータを取得します
+  // const apiBaseUrl = process.env.API_URL;
+  // const qiita = await fetch(`${apiBaseUrl}/api/qiita`);
+  // const data = blogs.contents.concat(await qiita.json());
+  // console.log(data);
   return {
     props: {
-      blog: data.contents,
+      blog: blogs.contents,
     },
   };
 };
