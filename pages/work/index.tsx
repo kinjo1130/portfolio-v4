07ae883b485179ea6b54efec672320b390d0da06
AFURO-1130 <abcshotaro616@gmail.com>
2024-05-04 22:37:48 +0900

    feat: workページの実装しました

--- pages/work/index.tsx
+++ pages/work/index.tsx
@@ -5,18 +5,22 @@ import { formatDate } from "@/libs/commom";
 
 export default function Work({ works }: { works: Works }) {
   return (
-    <Layout title="Work">
+    <Layout title="Work" tooltipText="インターンや業務委託の経験をまとめたもの">
       <ul>
         {works.map((work) => (
-          <li key={work.id}>
-            <p>{work.title}</p>
-            <div className="flex gap-4 items-center">
-              <p>{formatDate(work.fromAt)}</p>
+          <li key={work.id} className="my-10">
+            <h3 className="font-semibold text-lg">{work.title}</h3>
+            <div className="flex gap-2 items-center">
+              <p className="text-slate-400 font-semibold text-sm">
+                {formatDate(work.fromAt)}
+              </p>
               <p>-</p>
-              <p>{work.toAt ? formatDate(work.toAt) : "現在"} </p>
+              <p className="text-slate-400 font-semibold text-sm">
+                {work.toAt ? formatDate(work.toAt) : "現在"}{" "}
+              </p>
             </div>
-            <p>{work.description}</p>
-            <a href={work.link}>Link</a>
+            <p className="text-sm">{work.description}</p>
+            <a href={work.link} className="underline text-sm">会社情報</a>
           </li>
         ))}
       </ul>
