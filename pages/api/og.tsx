d1a06c6d508d90fece2831b3410aeab4081eb669
AFURO-1130 <abcshotaro616@gmail.com>
2024-05-06 11:41:56 +0900

    fix: OGPの高さの修正

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -42,7 +42,7 @@ export default async function handler(
         <div
         style={{
           position: "absolute",
-          bottom: 50,
+          bottom: 80,
           right: 10,
           display: "flex",
           alignItems: "center",
