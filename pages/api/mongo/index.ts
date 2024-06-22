2b9509266ba90b24d4c105f0fabfd38509aa741e
AFURO-1130 <abcshotaro616@gmail.com>
2024-06-23 00:05:21 +0900

    feat: 本番環境でも取得できるように早く帰ってくるようにした

--- pages/api/mongo/index.ts
+++ pages/api/mongo/index.ts
@@ -9,7 +9,7 @@ export default async function handler(
     try {
       const db = await connectToDatabase();
       const collection = db.collection("comments"); // コレクション名を指定
-      const data = await collection.find({}).toArray();
+      const data = await collection.findOne({ name: 'Ned Stark' });
       res.status(200).json(data);
     } catch (error) {
       res.status(500).json({ error: `Failed to fetch data ${error}` });
