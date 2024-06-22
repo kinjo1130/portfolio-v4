c203f459a71b1d51ce27dd454682053639e2e59f
AFURO-1130 <abcshotaro616@gmail.com>
2024-06-22 23:56:47 +0900

    feat: mongoDBをNext.jsで使えるようにしてみた

---
+++ pages/api/mongo/index.ts
@@ -0,0 +1,20 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { connectToDatabase } from './init';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'GET') {
+    try {
+      const db = await connectToDatabase();
+      const collection = db.collection("comments"); // コレクション名を指定
+      const data = await collection.find({}).toArray();
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(500).json({ error: `Failed to fetch data ${error}` });
+    }
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
+}
