6989ac8c28d886843f0260ab1c065f2de1eb5577
AFURO-1130 <abcshotaro616@gmail.com>
2024-06-23 00:13:43 +0900

    fix: DBの接続をプールする

--- pages/api/mongo/init.ts
+++ pages/api/mongo/init.ts
@@ -1,14 +1,4 @@
-// import { MongoClient } from 'mongodb';
-
-// const uri = 'mongodb+srv://portfolio-cms:portfolio-cms@portfoliocms.ypdmhne.mongodb.net/?retryWrites=true&w=majority&appName=portfolioCMS';
-// const dbName = 'sample_mflix'; // データベース名を指定
-// export const client = new MongoClient(uri);
-
-// export async function connectToDatabase() {
-//   if (client.connect) await client.connect();
-//   return client.db(dbName); // データベース名を指定
-// }
-import { MongoClient } from 'mongodb';
+import { Db, MongoClient } from 'mongodb';
 
 const MONGODB_USER = process.env.MONGODB_USER;
 const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
@@ -16,15 +6,27 @@ const MONGODB_CLUSTER = process.env.MONGODB_CLUSTER;
 const MONGODB_DATABASE = process.env.MONGODB_DATABASE;
 const MONGODB_APP_NAME = process.env.MONGODB_APP_NAME;
 
-if (!MONGODB_USER || !MONGODB_PASSWORD || !MONGODB_CLUSTER || !MONGODB_DATABASE) {
-  throw new Error('Please define the MONGODB_USER, MONGODB_PASSWORD, MONGODB_CLUSTER, and MONGODB_DATABASE environment variables inside .env.local');
+if (!MONGODB_USER || !MONGODB_PASSWORD || !MONGODB_CLUSTER || !MONGODB_DATABASE || !MONGODB_APP_NAME) {
+  throw new Error('Please define the MONGODB_USER, MONGODB_PASSWORD, MONGODB_CLUSTER, MONGODB_DATABASE, and MONGODB_APP_NAME environment variables inside .env.local');
 }
 
-const MONGODB_URI = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_CLUSTER}.ypdmhne.mongodb.net/?retryWrites=true&w=majority&appName=${MONGODB_APP_NAME}`;
+const MONGODB_URI = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_CLUSTER}.ypdmhne.mongodb.net/${MONGODB_DATABASE}?retryWrites=true&w=majority&appName=${MONGODB_APP_NAME}`;
 
-export const client = new MongoClient(MONGODB_URI);
+let cachedClient: MongoClient | null = null;
+let cachedDb: Db | null = null;
 
 export async function connectToDatabase() {
-  if (client.connect) await client.connect();
-  return client.db(MONGODB_DATABASE); // データベース名を指定
+  if (cachedClient && cachedDb) {
+    return { client: cachedClient, db: cachedDb };
+  }
+
+  const client = new MongoClient(MONGODB_URI);
+
+  await client.connect();
+  const db = client.db(MONGODB_DATABASE);
+
+  cachedClient = client;
+  cachedDb = db;
+
+  return { client, db };
 }
