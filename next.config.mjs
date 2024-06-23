bc35000916f94fdce1ad334c406cf4a103010fe3
AFURO-1130 <abcshotaro616@gmail.com>
2024-06-23 12:17:01 +0900

    CORSの設定追加

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,28 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value:
+              "https://portfolio-v4-gtcnj8gln-afuro1130s-projects.vercel.app", // 必要に応じてオリジンを変更
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST,PUT,DELETE,OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-Requested-With, Content-Type, Accept",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
