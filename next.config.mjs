70f8d7e46e728c1107def740fd3c175bd8cab985
AFURO-1130 <abcshotaro616@gmail.com>
2024-06-23 12:35:32 +0900

    cors修正

--- next.config.mjs
+++ next.config.mjs
@@ -8,28 +8,6 @@ const nextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value:
-              "portfolio-v4-git-feat-blog-like-ui-afuro1130s-projects.vercel.app",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,POST,PUT,DELETE,OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "X-Requested-With, Content-Type, Accept",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
--- pages/api/mongo/blog/getLike.ts
+++ pages/api/mongo/blog/getLike.ts
@@ -1,6 +1,23 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { connectToDatabase } from '../init';
+const allowedOrigins = process.env.NEXT_PUBLIC_ALLOWED_ORIGINS ? process.env.NEXT_PUBLIC_ALLOWED_ORIGINS.split(',') : [];
+
+const setCorsHeaders = (req: NextApiRequest, res: NextApiResponse) => {
+  const origin = req.headers.origin;
+  if (origin && allowedOrigins.some(allowedOrigin => new RegExp(allowedOrigin.replace('*', '.*')).test(origin))) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept');
+  }
+};
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  setCorsHeaders(req, res);
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
   if (req.method === 'GET') {
     try {
       const { db } = await connectToDatabase();
