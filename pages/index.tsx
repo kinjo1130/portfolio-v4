1bc359bfd7de8dda21b9c8b244d215e15952f974
AFURO-1130 <abcshotaro616@gmail.com>
2024-12-08 01:11:12 +0900

    biomeで怒られた部分の修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,54 +1,54 @@
+import Achievement from "@/components/Achievement";
+import Button from "@/components/Button";
+import MembershipList from "@/components/MembershipList";
+import SNS from "@/components/SNS";
+import { SeoHead } from "@/components/SeoHead";
+import Image from "next/image";
 import Link from "next/link";
 import Layout from "./layout";
-import Image from "next/image";
-import SNS from "@/components/SNS";
-import Button from "@/components/Button";
-import { SeoHead } from "@/components/SeoHead";
-import Achievement from "@/components/Achievement";
-import MembershipList from "@/components/MembershipList";
 export default function Home() {
-  return (
-    <>
-      <Layout title="Welcome!!">
-        <SeoHead
-          title="Home"
-          titleTemplate="Welcome!!"
-          description="Welcome to my portfolio site"
-          imgUrl="/favicon.ico"
-        ></SeoHead>
-        <div className="lg:grid lg:grid-cols-10">
-          <div className="col-span-5">
-            <Image
-              src="/profile.jpg"
-              alt="profile"
-              width={200}
-              height={200}
-              className="rounded-lg"
-            />
-            <p className="text-xl font-bold mt-5">
-              金城翔太郎 / ソフトウェアエンジニア
-            </p>
-            <p className="text-md font-semibold text-gray-400">
-              kinjo shotaro / Software Engineer
-            </p>
-          </div>
-          <div className="col-span-5">
-            <Achievement />
-            <div className="my-10">
-              <MembershipList />
-            </div>
-            <Button className="mt-10">
-              <Link href="/about">詳細なプロフィール</Link>
-            </Button>
-          </div>
-        </div>
+	return (
+		<>
+			<Layout title="Welcome!!">
+				<SeoHead
+					title="Home"
+					titleTemplate="Welcome!!"
+					description="Welcome to my portfolio site"
+					imgUrl="/favicon.ico"
+				/>
+				<div className="lg:grid lg:grid-cols-10">
+					<div className="col-span-5">
+						<Image
+							src="/profile.jpg"
+							alt="profile"
+							width={200}
+							height={200}
+							className="rounded-lg"
+						/>
+						<p className="text-xl font-bold mt-5">
+							金城翔太郎 / ソフトウェアエンジニア
+						</p>
+						<p className="text-md font-semibold text-gray-400">
+							kinjo shotaro / Software Engineer
+						</p>
+					</div>
+					<div className="col-span-5">
+						<Achievement />
+						<div className="my-10">
+							<MembershipList />
+						</div>
+						<Button className="mt-10">
+							<Link href="/about">詳細なプロフィール</Link>
+						</Button>
+					</div>
+				</div>
 
-        {/* SNS一覧 */}
-        <SNS />
-        {/* 屋号などの情報を掲載 */}
-      </Layout>
-    </>
-  );
+				{/* SNS一覧 */}
+				<SNS />
+				{/* 屋号などの情報を掲載 */}
+			</Layout>
+		</>
+	);
 }
 
 // // データをテンプレートに受け渡す部分の処理を記述します
