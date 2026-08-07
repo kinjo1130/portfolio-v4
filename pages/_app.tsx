65972546e7226ef1d34961398eb5a6dec06a7ccf
kinjo1130 <abcshotaro616@gmail.com>
2026-08-07 18:37:19 +0900

    feat: GA4計測を追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import "@/styles/globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import type { AppProps } from "next/app";
 
+// GA4 測定 ID (kinjyo.me / ストリーム 2657990993)。測定 ID は公開情報のため直書き。
+const GA_ID = "G-1M4CM8007S";
+
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Component {...pageProps} />
+			<GoogleAnalytics gaId={GA_ID} />
+		</>
+	);
 }
